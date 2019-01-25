809efbeb5b8eff5bf2a61f82771a6e26717b0dc1
kevinluu <this.kevinluu@gmail.com>
2019-01-24 22:50:52 -0500

    Added NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import NoMatch from './pages/NoMatch';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
+  <React.Fragment>
+    <NavBar />
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  </React.Fragment>
 );
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function Home() {
-  return (
-    <React.Fragment>
-      <h1>This is the home page</h1>
-    </React.Fragment>
-  );
+  return <h1>This is the home page</h1>;
 }
 
 export default Home;
